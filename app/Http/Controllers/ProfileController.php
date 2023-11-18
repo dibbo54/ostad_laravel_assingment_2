3e0cada7b331ccdc3a09e7584b6df000c2f3e093
dibbo54 <djs189426@gmail.com>
2023-11-18 12:10:24 +0600

    laravel
    laravel

---
+++ app/Http/Controllers/ProfileController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers;
+
+
+
+class ProfileController extends Controller
+{
+    public function index($id)
+    {
+        $name = "Donald Trump";
+        $age = "75";
+
+        $data = [
+            "id" => $id,
+            "name" => $name,
+            "age" => $age,
+        ];
+
+        
+        $cookieName = 'access_token';
+        $cookieValue = '123-XYZ';
+        $minutes = 1;
+        $path = '/';
+        $domain = $_SERVER['SERVER_NAME'];
+        $secure = false;
+        $httpOnly = true;
+
+        
+        $response =  response()->json($data, 200)->cookie($cookieName, $cookieValue, $minutes, $path, $domain, $secure, $httpOnly);
+       
+
+        return $response;
+    }
+}
